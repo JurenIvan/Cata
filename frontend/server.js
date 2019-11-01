ceb838cb513d1ce135ebb0d83701296caba2f151
jurenivan <juren.ivan@gmail.com>
2019-11-01 11:52:12 +0100

    change path to index.html

--- frontend/server.js
+++ frontend/server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/ui'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/ui/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/cata/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
