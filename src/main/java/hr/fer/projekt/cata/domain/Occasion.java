7612ac29e2b32d2753b45161642ff01ee8770959
jurenivan <juren.ivan@gmail.com>
2019-11-03 15:58:26 +0100

    add empty constructors

--- src/main/java/hr/fer/projekt/cata/domain/Occasion.java
+++ src/main/java/hr/fer/projekt/cata/domain/Occasion.java
@@ -2,12 +2,14 @@ package hr.fer.projekt.cata.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Occasion {
 
--- src/main/java/hr/fer/projekt/cata/domain/Users.java
+++ src/main/java/hr/fer/projekt/cata/domain/Users.java
@@ -2,12 +2,14 @@ package hr.fer.projekt.cata.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Users {
 
