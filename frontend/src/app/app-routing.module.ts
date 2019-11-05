3f7da084f78921b735f38ab8d405d8189a021612
zavrskina <petra.zavrski@infinum.hr>
2019-11-06 00:03:21 +0100

    add registration html

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FormRegistrationComponent} from "./form-registration/form-registration.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: "", component: FormRegistrationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormRegistrationComponent } from './form-registration/form-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormRegistrationComponent
   ],
   imports: [
     BrowserModule,
