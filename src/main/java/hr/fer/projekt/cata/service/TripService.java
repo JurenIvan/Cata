34fca3bb7f6aed970d7fd87b114c856ded53d460
jurenivan <juren.ivan@gmail.com>
2019-12-05 10:30:22 +0100

    enable joining trip

--- src/main/java/hr/fer/projekt/cata/service/TripService.java
+++ src/main/java/hr/fer/projekt/cata/service/TripService.java
@@ -2,12 +2,15 @@ package hr.fer.projekt.cata.service;
 
 import hr.fer.projekt.cata.config.errorHandling.CATAException;
 import hr.fer.projekt.cata.domain.Trip;
+import hr.fer.projekt.cata.domain.User;
 import hr.fer.projekt.cata.repository.TripPlanRepository;
 import hr.fer.projekt.cata.repository.TripRepository;
 import hr.fer.projekt.cata.web.rest.dto.TripDto;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -32,13 +35,15 @@ public class TripService {
 
     public TripDto createTrip(TripDto tripDto) {
         var tripPlan = tripPlanRepository.findById(tripDto.getTripPlanDto().getId()).orElseThrow(() -> new CATAException());
-        Trip trip = new Trip(null, tripDto.getStartDateTime(), tripDto.getEndDateTime(), tripDto.getPrice(), tripDto.getPassengerCount(), tripPlan);
+        Trip trip = new Trip(null, tripDto.getStartDateTime(), tripDto.getEndDateTime(), tripDto.getPrice(), tripDto.getPassengerCount(), new ArrayList<>(), tripPlan);
         return tripRepository.save(trip).toDto();
     }
 
     public TripDto joinTrip(long tripId) {
-        var trip=tripRepository.findById(tripId).orElseThrow(()->new CATAException());
-        //todo
-        return null;
+        var trip = tripRepository.findById(tripId).orElseThrow(() -> new CATAException());
+        User currUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        trip.addPassenger(currUser);
+        tripRepository.save(trip);
+        return trip.toDto();
     }
 }
