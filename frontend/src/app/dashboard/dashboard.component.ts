ab13fafaff538269ecb28c61e4c88dfeb52847d1
zavrskina <petra.zavrski@infinum.hr>
2019-12-08 18:37:51 +0100

    add show add trip button for admins

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Trip} from "../../models/trip";
 import {TravelService} from "../../services/travel.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -11,11 +12,20 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class DashboardComponent implements OnInit {
 
   public travelList: Trip[];
+  private isAdmin: boolean
 
-  constructor(private travelService: TravelService, private route: ActivatedRoute, private router: Router) {
+
+  constructor(private travelService: TravelService, private userService: UserService, private route: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit() {
+
+    this.userService.isUserAdmin().subscribe(
+      isAdmin => {
+        this.isAdmin = isAdmin;
+        console.log(isAdmin)
+      }
+    );
 
     this.travelService.loadTravels();
 
